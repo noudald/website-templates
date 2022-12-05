dbd47e09b8902356740ad622ce1b38dc1130c7cd
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2022-12-05 11:26:11 +0100

    Shuffle questions

--- risk-analysis/main.js
+++ risk-analysis/main.js
@@ -1,13 +1,30 @@
 const question = document.getElementById('question');
 const answer = document.answerForm.answer;
 
+function shuffleArray(arr) {
+  // Fisher-Yates shuffle
+  let curIndex = arr.length;
+  let randIndex = 0;
+
+  while (curIndex != 0) {
+    randIndex = Math.floor(Math.random() * curIndex);
+    curIndex--;
+
+    [arr[curIndex], arr[randIndex]] = [arr[randIndex], arr[curIndex]];
+  }
+
+  return arr;
+}
+
+const questionsRand = shuffleArray(questions);
+
 currentQuestionIndex = 0;
-question.innerHTML = questions[currentQuestionIndex][0];
+question.innerHTML = questionsRand[currentQuestionIndex][0];
 
 var results = [];
 for (var i = 0; i < answer.length; i++) {
   answer[i].addEventListener('change', function() {
-    results.push([parseInt(this.value), questions[currentQuestionIndex][1]]);
+    results.push([parseInt(this.value), questionsRand[currentQuestionIndex][1]]);
     this.checked = false;
 
     const calibration = [...Array(11).keys()].map(i =>  {
@@ -24,6 +41,6 @@ for (var i = 0; i < answer.length; i++) {
     chart.update();
 
     currentQuestionIndex++;
-    question.innerHTML = questions[currentQuestionIndex][0];
+    question.innerHTML = questionsRand[currentQuestionIndex][0];
   });
 }
